903313304aeb043cbe6d3e5984667c5121bfff21
dhanushv2000 <66072205+dhanushv2000@users.noreply.github.com>
2020-07-09 21:45:03 +0530

    done

--- Read NEI and SCC Data.R
+++ Read NEI and SCC Data.R
@@ -1,6 +1,12 @@
-## Step# 02
-##
-## Read SCC and NEI RDS files - which may consume time
-##
+url1 <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+
+destfile1 <- "destfile.zip"
+
+if(!file.exists(destfile1)) {
+  download.file(url1,
+                destfile = destfile1,
+                method = "curl")
+  unzip(destfile1, exdir = ".")
+}
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
